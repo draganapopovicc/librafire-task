c2111dc486a0100c10fcb125b5c35d4fd87069d9
DraganaDragana <draganapopovic.123123@gmail.com>
2023-06-10 20:18:50 +0200

    Add logic for play/pause video & audio

--- src/js/app.js
+++ src/js/app.js
@@ -26,3 +26,41 @@ if (document.querySelector('.b-btn')) {
     });
   });
 }
+
+//Play/pause video and audio
+if (document.querySelector('.myUQ-video')) {
+  document.querySelectorAll('.myUQ__controls-video').forEach((videoControl) => {
+    videoControl.addEventListener('click', (e) => {
+      let buttonsWrap = e.target.closest('.myUQ__controls');
+      let videoWrap = e.target.closest('.myUQ__video');
+
+      if (buttonsWrap.classList.contains('play-video')) {
+        videoWrap.querySelector('.myUQ-video').pause();
+        videoWrap.querySelector('.myUQ-audio').pause();
+        buttonsWrap.classList.remove('play-video');
+      } else {
+        videoWrap.querySelector('.myUQ-video').play();
+        videoWrap.querySelector('.myUQ-audio').play();
+        buttonsWrap.classList.add('play-video');
+        buttonsWrap.querySelector('.myUQ__controls-sound').classList.remove('no-sound');
+      }
+    });
+  });
+
+  document.querySelectorAll('.myUQ__controls-sound').forEach((soundControl) => {
+    soundControl.addEventListener('click', (e) => {
+      let playSound = e.target.closest('.myUQ__controls-sound');
+      let videoWrap = e.target.closest('.myUQ__video');
+
+      if (videoWrap.querySelector('.myUQ__controls').classList.contains('play-video')) {
+        if (!playSound.classList.contains('no-sound')) {
+          videoWrap.querySelector('.myUQ-audio').pause();
+          playSound.classList.add('no-sound');
+        } else {
+          videoWrap.querySelector('.myUQ-audio').play();
+          playSound.classList.remove('no-sound');
+        }
+      }
+    });
+  });
+}
